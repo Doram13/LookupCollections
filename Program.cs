35cbfa38a023a69def06844306b1ee190c475a1b
Doram13 <1991doram@gmail.com>
2019-03-21 13:58:01 +0100

    ListDictionary implemented

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
 
 namespace LookupCollections
 {
@@ -10,8 +13,14 @@ namespace LookupCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            ListDictionary countriesDict = new ListDictionary(
+                StringComparer.InvariantCultureIgnoreCase);
+            countriesDict.Add("Estados Unidos", "United States");
+            countriesDict.Add("Canadá", "Canada");
+            countriesDict.Add("España", "Spain");
+            Console.WriteLine(countriesDict["Canadá"]);
+            Console.WriteLine(countriesDict["España"]);
+            Console.ReadLine();
         }
     }
 }
